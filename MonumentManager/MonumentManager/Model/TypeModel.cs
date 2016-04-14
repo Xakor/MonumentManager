9a5b7a64f04433ff056e2b71579349a88afb29ee
Frank Andersen <fha.post@gmail.com>
2016-04-14 10:39:32 +0200

    Created our various data models, deleted temp.txt in model folder as it was no longer nescersary!

---
+++ MonumentManager/MonumentManager/Model/TypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonumentManager.Model
+{
+    class TypeModel
+    {
+        //Properties
+        public int id { get; set; }
+        public string name { get; set; } 
+    }
+}
