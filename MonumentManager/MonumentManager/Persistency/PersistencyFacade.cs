a4c1b2651a2baec4c52c309a6ea6393ef3110486
DIANA <burasdiana@hotmail.com>
2016-05-10 13:57:37 +0200

    fixed persservice

---
+++ MonumentManager/MonumentManager/Persistency/PersistencyFacade.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.ComTypes;
+using MonumentManager.Model;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+using Newtonsoft.Json;
+
+
+namespace MonumentManager.Persistency
+{
+    class PersistencyFacade
+    {
+        const string ServerUrl = "http://localhost:50000";
+        HttpClientHandler handler;
+
+        public void PersistenceFacade()
+        {
+            handler = new HttpClientHandler();
+            handler.UseDefaultCredentials = true;
+        }
+
+        public List<SculptureModel> GetSculptures()
+        {
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    var response = client.GetAsync("api/Sculptures").Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var sculptureList = response.Content.ReadAsAsync<IEnumerable<SculptureModel>>().Result;
+                        return sculptureList.ToList();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    new MessageDialog(ex.Message).ShowAsync();
+                }
+                return null;
+            }
+
+        }
+
+        public void SaveSculpture(SculptureModel sculpture)
+        {
+            using (var client = new HttpClient(handler))
+            {
+                client.BaseAddress = new Uri(ServerUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                try
+                {
+                    string postBody = JsonConvert.SerializeObject(sculpture);
+                    var response =
+                        client.PostAsync("api/Sculptures",
+                            new StringContent(postBody, Encoding.UTF8, "application/json")).Result;
+
+                }
+                catch (Exception ex)
+                {
+                    new MessageDialog(ex.Message).ShowAsync();
+                }
+            }
+
+        }
+    }
+}
